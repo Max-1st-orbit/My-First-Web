3194b7263b8ecb74dce2c1faba846ef103d29da4
Mcrafter15 Marx kap <Kapambwemarx@gmail.com>
2025-06-14 15:54:13 +0200

    Updated site

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 
-const Mbutton = document.getElementById("2");
-const Lbutton = document.getElementById("1");
-const Vbutton = document.getElementById("3");
+const Home = document.getElementById("home");
+const Projects = document.getElementById("proj");
+const About = document.getElementById("abou");
+const contact = document.getElementById("cont");
 
-Mbutton.addEventListener('click', () => {alert('you clicked a button!');});
-Lbutton.addEventListener('click', () => {window.location.href = "life.html"});
-Vbutton.addEventListener('click', () => {window.location.href = "videoGames.html"});
+Home.addEventListener('click', () => {window.location.href = "index.html"});
+Projects.addEventListener('click', () => {window.location.href = "videoGames.html"});
+About.addEventListener('click', () => {window.location.href = "life.html"});
+contact.addEventListener('click', () => {window.location.href = "Contact.html"});
